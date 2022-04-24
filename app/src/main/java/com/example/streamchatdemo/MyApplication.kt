609a36f0d4e47be9aa1a63a667a0f663b59ddb2e
Naman Rana <namanrana1828@gmail.com>
2022-04-24 13:39:00 +0530

    Initial commit - V1

---
+++ app/src/main/java/com/example/streamchatdemo/MyApplication.kt
@@ -0,0 +1,20 @@
+package com.example.streamchatdemo
+
+import android.app.Application
+
+
+
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.logger.ChatLogLevel
+import io.getstream.chat.android.livedata.ChatDomain
+import streamchatdemo.R
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val client =
+            ChatClient.Builder(getString(R.string.api_key), this).logLevel(ChatLogLevel.ALL).build()
+        ChatDomain.Builder(client, this).build()
+    }
+}
